af913349649914aa180c881eb54d1af83c2dd2d7
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2022-11-29 01:35:15 +0200

    fix seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(TranslationSeed::class);
+        $this->call(\database\seeds\TranslationSeeder::class);
     }
 }
